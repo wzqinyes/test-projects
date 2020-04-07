98b69b374a9a9adf5d96ef258cc7e082c17cdf5a
wuzeqin <wzqinyes@163.com>
2020-04-07 23:43:46 +0800

    填充。。。

--- mybatis-demo/src/main/java/com/example/demo/configuration/DefaultMetaObjectHandler.java
+++ mybatis-demo/src/main/java/com/example/demo/configuration/DefaultMetaObjectHandler.java
@@ -4,7 +4,7 @@ import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
+import java.util.Date;
 
 /**
  * 自动填充默认字段，比如创建时间、修改时间、创建人、修改人
@@ -16,15 +16,11 @@ public class DefaultMetaObjectHandler implements MetaObjectHandler {
 
     @Override
     public void insertFill(MetaObject metaObject) {
-        if (metaObject.hasSetter(CREATE_TIME_FIELD_NAME)) {
-            setInsertFieldValByName(CREATE_TIME_FIELD_NAME, LocalDateTime.now(), metaObject);
-        }
+        setInsertFieldValByName(CREATE_TIME_FIELD_NAME, new Date(), metaObject);
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        if (metaObject.hasSetter(CREATE_TIME_FIELD_NAME)) {
-            setUpdateFieldValByName(CREATE_TIME_FIELD_NAME, LocalDateTime.now(), metaObject);
-        }
+        setInsertFieldValByName(CREATE_TIME_FIELD_NAME, new Date(), metaObject);
     }
 }
