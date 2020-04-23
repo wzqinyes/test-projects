b095ca01b215ab96e1863eca666fa85e661ffeb0
wuzeqin <wuzeqin@iot-cas.com>
2020-04-23 11:47:05 +0800

    mongodb

---
+++ mongodb/src/main/java/com/example/mongodb/domain/UserTTT.java
@@ -0,0 +1,32 @@
+package com.example.mongodb.domain;
+
+public class UserTTT {
+    private int id;
+    private String name;
+    private String sex;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
---
+++ mongodb/src/main/java/com/example/mongodb/web/MongoController.java
@@ -0,0 +1,4 @@
+package com.example.mongodb.web;
+
+public class MongoController {
+}
